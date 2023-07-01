7d412779fc148d11bd4c864ca447fa71c4df4695
ahlixinjie <ahlixinjie@gmail.com>
2023-07-01 18:37:32 +0800

    add list converter

--- collections/list/list.go
+++ collections/list/list.go
@@ -1,1 +1,35 @@
 package list
+
+import "reflect"
+
+type convertOption struct {
+	ignoreZero bool
+}
+
+type ConvertOption func(*convertOption)
+
+func WithIgnoreZero() ConvertOption {
+	return func(option *convertOption) {
+		option.ignoreZero = true
+	}
+}
+
+// Converter will convert one type of slice to another type of slice.
+func Converter[SOURCE any, DEST comparable](f func(SOURCE) DEST, list []SOURCE, opts ...ConvertOption) (result []DEST) {
+	var opt convertOption
+	for _, o := range opts {
+		o(&opt)
+	}
+
+	result = make([]DEST, 0, len(list))
+
+	for _, v := range list {
+		r := f(v)
+
+		if opt.ignoreZero && reflect.ValueOf(r).IsZero() {
+			continue
+		}
+		result = append(result, r)
+	}
+	return
+}
