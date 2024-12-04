063e4c80cda1416e1cb79bf7487c22a3f7b292cb
Xinjie Li <ahlixinjie@gmail.com>
2024-12-04 16:01:46 +0800

    add support for batch parallel

--- batch/batch.go
+++ batch/batch.go
@@ -1,30 +1,198 @@
 package batch
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"runtime/debug"
+	"sync"
+	"time"
+)
 
-// DoBatchFunc is called for each batch.
-// Any error will cancel the batching operation but returning ErrAbort
+// BatchFunc is called for each batch.
+// Any error will cancel the batching operation but returning Abort
 // indicates it was deliberate, and not an error case.
 // [start:end)
-type DoBatchFunc func(start, end int) error
+type BatchFunc func(start, end int) error
 
-// ErrAbort indicates a batch operation should abort early.
-var ErrAbort = errors.New("done")
+type ParallelBatchFunc func(ctx context.Context, start, end int) error
+
+// Abort indicates a batch operation should abort early.
+// nolint:golint
+var (
+	Abort      = errors.New("done")
+	defaultErr = errors.New("default")
+	nilErr     = errors.New("nil")
+)
 
 // All calls eachFn for all items
-// Returns any error from eachFn except for ErrAbort it returns nil.
-func All(count, batchSize int, eachFn DoBatchFunc) error {
+// Returns any error from eachFn except for Abort it returns nil.
+func All(count, batchSize int, eachFn BatchFunc) error {
 	for i := 0; i < count; i += batchSize {
 		end := i + batchSize
 		if end > count {
 			end = count
 		}
 		err := eachFn(i, end)
-		if err == ErrAbort {
+		if err == Abort {
 			return nil
 		}
 		if err != nil {
 			return err
+		}
+	}
+	return nil
+}
+
+// parallelOptions holds the options for the Parallel function.
+type parallelOptions struct {
+	timeout time.Duration // Timeout duration for the entire execution
+}
+
+// WithTimeout sets the timeout duration for the Parallel function.
+// In case the ctx context.Context already has a deadline / timeout,
+// adding the longer timeout will not be applied.
+func WithTimeout(timeout time.Duration) func(*parallelOptions) {
+	return func(opts *parallelOptions) {
+		opts.timeout = timeout
+	}
+}
+
+func Parallel(
+	ctx context.Context,
+	count, batchSize, maxWorker int,
+	eachFn ParallelBatchFunc,
+	options ...func(*parallelOptions),
+) error {
+	if count == 0 {
+		return nil
+	}
+	maxWorker, totalInvokeCount, err := splitParallel(count, batchSize, maxWorker)
+	if err != nil {
+		return err
+	}
+	// Create a parallelOptions struct and apply any specified options
+	opts := &parallelOptions{
+		timeout: 0, // Default timeout duration (no timeout)
+	}
+	for _, opt := range options {
+		opt(opts)
+	}
+	return parallelInternal(ctx, count, maxWorker, totalInvokeCount, batchSize, eachFn, opts.timeout)
+}
+
+func splitParallel(count, batchSize, maxWorker int) (int, int, error) {
+	if count == 0 {
+		return 0, 0, nil
+	}
+	if batchSize <= 0 {
+		return 0, 0, errors.New("parallel batch size cannot be 0")
+	}
+	if maxWorker <= 0 {
+		return 0, 0, errors.New("max worker cannot be 0")
+	}
+
+	totalInvokeCount := count / batchSize // how many times eachFn is called
+	if count%batchSize != 0 {             // have leftovers, increase totalInvokeCount by 1
+		totalInvokeCount++
+	}
+	if maxWorker > totalInvokeCount { // some workers will not get a job, let's make maxWorker = totalInvokeCount
+		maxWorker = totalInvokeCount
+	}
+
+	return maxWorker, totalInvokeCount, nil
+}
+
+func overwriteTimeoutIfSet(
+	ctx context.Context,
+	timeout time.Duration,
+) (context.Context, context.CancelFunc) {
+	// When we want to overwrite timeout in ctx
+	if timeout > 0 {
+		return context.WithTimeout(ctx, timeout)
+	}
+	// When the deadline in ctx is set, use it
+	if _, deadlineSet := ctx.Deadline(); deadlineSet {
+		return context.WithCancel(ctx)
+	}
+	// No timeout
+	return context.WithCancel(ctx)
+}
+
+// nolint:funlen
+func parallelInternal(
+	ctx context.Context,
+	count, maxWorker, totalInvokeCount, batchSize int,
+	eachFn ParallelBatchFunc,
+	timeout time.Duration,
+) error {
+	dataChan := make(chan int, totalInvokeCount)
+	errChan := make(chan error, totalInvokeCount)
+
+	batchCtx, cancel := overwriteTimeoutIfSet(ctx, timeout)
+	defer cancel()
+
+	var wg sync.WaitGroup
+	wg.Add(maxWorker)
+
+	for worker := 0; worker < maxWorker; worker++ {
+		go func() {
+			err := defaultErr
+			defer wg.Done()
+			defer func() {
+				r := recover()
+				if r != nil {
+					errChan <- errors.New(fmt.Sprintf("parallel batch panic recovered: %v, %s", r, string(debug.Stack())))
+					return
+				}
+				//To handle the runtime.Goexit() of gomock
+				//This means the eachFn exit and doesn't throw any panic
+				if errors.Is(defaultErr, err) {
+					errChan <- errors.New(fmt.Sprintf("goroutine exited without panic, %s", string(debug.Stack())))
+				}
+			}()
+			for {
+				select {
+				case <-batchCtx.Done():
+					err = nilErr
+					return
+				case start, hasJob := <-dataChan:
+					if !hasJob {
+						err = nilErr
+						return
+					}
+					end := start + batchSize
+					if end > count {
+						end = count
+					}
+					err = eachFn(batchCtx, start, end)
+					errChan <- err
+				}
+			}
+		}()
+	}
+
+	for i := 0; i < count; i += batchSize {
+		dataChan <- i
+	}
+	close(dataChan)
+
+	wg.Wait()
+
+	// Close the error channel to signal that no more errors will be sent
+	close(errChan)
+
+	finishCount := 0
+	for err := range errChan {
+		if err != nil {
+			if errors.Is(err, Abort) {
+				return nil
+			}
+			return err
+		}
+		finishCount++
+		if finishCount >= totalInvokeCount {
+			return nil
 		}
 	}
 	return nil
--- batch/batch_test.go
+++ batch/batch_test.go
@@ -1,13 +1,20 @@
 package batch
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"reflect"
+	"runtime"
+	"strconv"
+	"sync/atomic"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestExampleAll(t *testing.T) {
+func ExampleAll() {
 	ids := []int{1, 2, 3, 4, 5, 6, 7, 8}
 	var output []int
 	err := All(len(ids), 5, func(start, end int) error {
@@ -121,7 +128,7 @@ func TestAbort(t *testing.T) {
 			start: start,
 			end:   end,
 		})
-		return ErrAbort
+		return Abort
 	})
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
@@ -150,7 +157,390 @@ func TestErr(t *testing.T) {
 		})
 		return errTest
 	})
-	if err != errTest {
+	if !errors.Is(err, errTest) {
 		t.Errorf("Unexpected error: %v", err)
 	}
+}
+
+func Test_Parallel_OkCase(t *testing.T) {
+	type testCase struct {
+		name      string
+		count     int
+		batchSize int
+		maxWorker int
+		err       error
+	}
+	testCases := []testCase{
+		{name: "parallel 1", count: 122, batchSize: 10, maxWorker: 4},
+		{name: "parallel 2", count: 122, batchSize: 30, maxWorker: 100},
+		{name: "parallel 3", count: 418, batchSize: 7, maxWorker: 13},
+		{name: "parallel 4", count: 122, batchSize: 9, maxWorker: 10},
+		{name: "parallel 5", count: 152, batchSize: 10, maxWorker: 10},
+		{name: "parallel 6", count: 11, batchSize: 10, maxWorker: 10},
+		{name: "parallel 7", count: 0, batchSize: 1, maxWorker: 1},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			looped := make([]int, tc.count)
+
+			err := Parallel(context.Background(), tc.count, tc.batchSize, tc.maxWorker, func(ctx context.Context, start, end int) error {
+				for i := start; i < end; i++ {
+					looped[i] += 1
+				}
+				return nil
+			})
+			assert.NoError(t, err)
+
+			for i, loop := range looped {
+				assert.Equal(t, 1, loop, "index: "+strconv.Itoa(i))
+			}
+		})
+	}
+}
+
+func Test_Parallel_ErrorCase(t *testing.T) {
+	type testCase struct {
+		name      string
+		count     int
+		batchSize int
+		maxWorker int
+		err       error
+	}
+	testCases := []testCase{
+		{name: "0 batch size", count: 10, batchSize: 0, maxWorker: 10},
+		{name: "0 max worker", count: 10, batchSize: 10, maxWorker: 0},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			looped := make([]int, tc.count)
+			for i := range looped {
+				looped[i] = 0
+			}
+
+			err := Parallel(context.Background(), tc.count, tc.batchSize, tc.maxWorker, func(ctx context.Context, start, end int) error {
+				return nil
+			})
+			assert.Error(t, err)
+		})
+	}
+
+	t.Run("batch err", func(t *testing.T) {
+		err := Parallel(context.Background(), 100, 1, 10, func(ctx context.Context, start, end int) error {
+			return errors.New("batch err")
+		})
+		assert.Error(t, err)
+	})
+
+	t.Run("abort early", func(t *testing.T) {
+		err := Parallel(context.Background(), 100, 1, 10, func(ctx context.Context, start, end int) error {
+			return Abort
+		})
+		assert.NoError(t, err)
+	})
+
+	t.Run("panic", func(t *testing.T) {
+		counter := int32(0)
+		err := Parallel(context.Background(), 100, 10, 10, func(ctx context.Context, start, end int) error {
+			if atomic.AddInt32(&counter, 1) == 1 {
+				panic("some panic here")
+			}
+			return nil
+		})
+		assert.Error(t, err)
+	})
+
+	// gomock uses this, and it will cause deadlock
+	t.Run("goexit", func(t *testing.T) {
+		counter := int32(0)
+		err := Parallel(context.Background(), 100, 10, 10, func(ctx context.Context, start, end int) error {
+			if atomic.AddInt32(&counter, 1) == 1 {
+				runtime.Goexit()
+			}
+			return nil
+		})
+		assert.Error(t, err)
+	})
+}
+
+func TestParallel(t *testing.T) {
+	// Define a test case with a batch function that sleeps for a specific duration
+	testCase := struct {
+		count     int
+		batchSize int
+		maxWorker int
+		sleepTime time.Duration
+	}{
+		count:     10,
+		batchSize: 3,
+		maxWorker: 5,
+		sleepTime: 100 * time.Millisecond,
+	}
+
+	// Define the batch function for the test case
+	batchFn := func(ctx context.Context, start, end int) error {
+		select {
+		case <-ctx.Done():
+			return ctx.Err() // Return context error if canceled
+		default:
+			// Simulate work by sleeping for a specified duration
+			time.Sleep(testCase.sleepTime)
+			fmt.Printf("Processed batch [%d, %d)\n", start, end)
+			return nil
+		}
+	}
+
+	// Run the Parallel function with the test case configuration
+	err := Parallel(context.Background(), testCase.count, testCase.batchSize, testCase.maxWorker, batchFn)
+	assert.NoError(t, err)
+}
+
+func TestParallel_WithErrors(t *testing.T) {
+	// Define a test case with a batch function that returns an error for a specific range
+	testCase := struct {
+		count      int
+		batchSize  int
+		maxWorker  int
+		errorRange [2]int
+	}{
+		count:      10,
+		batchSize:  3,
+		maxWorker:  5,
+		errorRange: [2]int{4, 7}, // Causes an error for range [4, 7)
+	}
+
+	// Define the batch function for the test case
+	batchFn := func(ctx context.Context, start, end int) error {
+		select {
+		case <-ctx.Done():
+			return ctx.Err() // Return context error if canceled
+		default:
+			// Simulate work
+			fmt.Printf("Processing batch [%d, %d)\n", start, end)
+
+			// Simulate an error for the specified range
+			if start >= testCase.errorRange[0] && start < testCase.errorRange[1] {
+				return errors.New("error occurred in batch")
+			}
+
+			return nil
+		}
+	}
+
+	// Run the Parallel function with the test case configuration
+	err := Parallel(context.Background(), testCase.count, testCase.batchSize, testCase.maxWorker, batchFn)
+	assert.Error(t, err)
+	assert.EqualError(t, err, "error occurred in batch")
+}
+
+func TestParallel_CancelContext(t *testing.T) {
+	// Define a test case where the context is canceled before completing the batches
+	testCase := struct {
+		count     int
+		batchSize int
+		maxWorker int
+	}{
+		count:     10,
+		batchSize: 3,
+		maxWorker: 5,
+	}
+
+	// Define the batch function for the test case
+	batchFn := func(ctx context.Context, start, end int) error {
+		select {
+		case <-ctx.Done():
+			return ctx.Err() // Return context error if canceled
+		default:
+			// Simulate work
+			fmt.Printf("Processing batch [%d, %d)\n", start, end)
+			return nil
+		}
+	}
+
+	// Create a cancellation context and cancel it immediately
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	// Run the Parallel function with the canceled context and test case configuration
+	err := Parallel(ctx, testCase.count, testCase.batchSize, testCase.maxWorker, batchFn)
+	assert.Error(t, err)
+	assert.EqualError(t, err, "context canceled")
+}
+
+func TestWorkerStarvationWithTimeout(t *testing.T) {
+	// Create a context with a timeout for the batch
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Set up the necessary variables for parallel execution
+	count := 10    // Total number of tasks
+	batchSize := 2 // Number of tasks processed in each batch
+	maxWorker := 2 // Maximum number of workers
+
+	// Define a ParallelBatchFunc that performs a task that never completes
+	eachFn := func(ctx context.Context, start, end int) error {
+		// Simulate a task that blocks indefinitely
+		<-ctx.Done()
+		return nil
+	}
+
+	// Call the Parallel function and expect a deadlock error
+	err := Parallel(ctx, count, batchSize, maxWorker, eachFn, WithTimeout(1*time.Second))
+	assert.NoError(t, err)
+}
+
+func TestWorkerWithTimeout(t *testing.T) {
+	// Create a context with a timeout for the batch
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// Set up the necessary variables for parallel execution
+	count := 10    // Total number of tasks
+	batchSize := 2 // Number of tasks processed in each batch
+	maxWorker := 2 // Maximum number of workers
+
+	// Define a ParallelBatchFunc that performs a task that never completes
+	eachFn := func(ctx context.Context, start, end int) error {
+		// Simulate a task that blocks indefinitely
+		<-ctx.Done()
+		return nil
+	}
+
+	// Call the Parallel function and expect a deadlock error
+	err := Parallel(ctx, count, batchSize, maxWorker, eachFn)
+	assert.NoError(t, err)
+}
+
+func TestInadequateWorkerCount(t *testing.T) {
+	// Create a context with a cancellation function
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Set up the necessary variables for parallel execution
+	count := 10    // Total number of tasks
+	batchSize := 2 // Number of tasks processed in each batch
+	maxWorker := 2 // Maximum number of workers
+
+	// Define a ParallelBatchFunc that returns Abort after processing a task
+	eachFn := func(ctx context.Context, start, end int) error {
+		// Simulate a task that aborts early
+		if start == 2 {
+			return Abort
+		}
+		// Simulate other tasks that take some time to complete
+		time.Sleep(100 * time.Millisecond)
+		return nil
+	}
+
+	// Call the Parallel function and expect an error due to inadequate workers
+	err := Parallel(ctx, count, batchSize, maxWorker, eachFn)
+	assert.NoError(t, err)
+}
+
+func TestGoRoutineFn(t *testing.T) {
+	// Create a context with a cancellation function
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Set up the necessary variables for parallel execution
+	count := 10    // Total number of tasks
+	batchSize := 2 // Number of tasks processed in each batch
+	maxWorker := 2 // Maximum number of workers
+
+	// Define a ParallelBatchFunc that performs a go routine task
+	eachFn := func(ctx context.Context, start, end int) error {
+		go func() {
+			time.Sleep(1 * time.Second)
+		}()
+		return nil
+	}
+
+	// Call the Parallel function and expect a deadlock error
+	err := Parallel(ctx, count, batchSize, maxWorker, eachFn)
+	assert.NoError(t, err)
+}
+
+func benchmarkParallel(b *testing.B, count, batchSize, maxWorker int) {
+	// Define the batch function for the benchmark
+	batchFn := func(ctx context.Context, start, end int) error {
+		// Simulate work by sleeping for a small duration
+		time.Sleep(10 * time.Millisecond)
+		return nil
+	}
+
+	// Run the Parallel function in the benchmark
+	for n := 0; n < b.N; n++ {
+		_ = Parallel(context.Background(), count, batchSize, maxWorker, batchFn)
+	}
+}
+
+// Before:
+// goos: darwin
+// goarch: amd64
+// pkg: git.garena.com/shopee/promotion/promotion-common/batch
+// cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+// Benchmark_Parallel_10_1_1-12    	      10	 104547270 ns/op	     768 B/op	       8 allocs/op
+
+// After:
+// goos: darwin
+// goarch: amd64
+// pkg: git.garena.com/shopee/promotion/promotion-common/batch
+// cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+// === RUN   Benchmark_Parallel_10_1_1
+// Benchmark_Parallel_10_1_1
+// Benchmark_Parallel_10_1_1-12                  10         104460434 ns/op             956 B/op         13 allocs/op
+func Benchmark_Parallel_10_1_1(b *testing.B) {
+	benchmarkParallel(b, 10, 1, 1)
+}
+
+// Before:
+// goos: darwin
+// goarch: amd64
+// pkg: git.garena.com/shopee/promotion/promotion-common/batch
+// cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+// Benchmark_Parallel_10_5_5-12    	     100	  10669803 ns/op	     755 B/op	      10 allocs/op
+
+// After:
+// goos: darwin
+// goarch: amd64
+// pkg: git.garena.com/shopee/promotion/promotion-common/batch
+// cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+// Benchmark_Parallel_10_5_5-12    	     100	  10495422 ns/op	     956 B/op	      15 allocs/op
+func Benchmark_Parallel_10_5_5(b *testing.B) {
+	benchmarkParallel(b, 10, 5, 5)
+}
+
+// Before:
+// goos: darwin
+// goarch: amd64
+// pkg: git.garena.com/shopee/promotion/promotion-common/batch
+// cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+// Benchmark_Parallel_1000_100_10-12    	     100	  10681186 ns/op	    2600 B/op	      27 allocs/op
+
+// After:
+// goos: darwin
+// goarch: amd64
+// pkg: git.garena.com/shopee/promotion/promotion-common/batch
+// cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+// Benchmark_Parallel_1000_100_10-12    	     100	  10521900 ns/op	    2947 B/op	      32 allocs/op
+func Benchmark_Parallel_1000_100_10(b *testing.B) {
+	benchmarkParallel(b, 1000, 100, 10)
+}
+
+// Before:
+// goos: darwin
+// goarch: amd64
+// pkg: git.garena.com/shopee/promotion/promotion-common/batch
+// cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+// Benchmark_Parallel_10000_1000_100-12    	     100	  10684849 ns/op	    2567 B/op	      27 allocs/op
+
+// After:
+// goos: darwin
+// goarch: amd64
+// pkg: git.garena.com/shopee/promotion/promotion-common/batch
+// cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+// Benchmark_Parallel_10000_1000_100-12    	     100	  10754753 ns/op	    2997 B/op	      33 allocs/op
+func Benchmark_Parallel_10000_1000_100(b *testing.B) {
+	benchmarkParallel(b, 10000, 1000, 100)
 }
